d4568c4308b4ee51e057b3a54affa7c1f681cad8
NethminiKarunarathne <e20189@eng.pdn.ac.lk>
2024-07-15 10:26:30 +0530

    [59] updating the adminAuth controller

--- ocrspring/src/main/java/com/oasis/ocrspring/controller/adminAuth.java
+++ ocrspring/src/main/java/com/oasis/ocrspring/controller/adminAuth.java
@@ -1,11 +1,24 @@
 package com.oasis.ocrspring.controller;
 
+import com.oasis.ocrspring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/admin/auth")
 public class adminAuth {
+    @Autowired
+    private UserRepository UserRepo;
+    @ApiIgnore
+    public void redirrect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swaggr-ui.html");
+    }
     @PostMapping("/signup")
     public String adminSignUp(){
         return "/api/admin/auth/signup ";
