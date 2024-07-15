592e6628b9b8a120c654201733ad095755cb5c86
NethminiKarunarathne <e20189@eng.pdn.ac.lk>
2024-07-15 15:21:36 +0530

    [73] create /api/dashboard controller

---
+++ ocrspring/src/main/java/com/oasis/ocrspring/controller/Dashboard.java
@@ -0,0 +1,38 @@
+package com.oasis.ocrspring.controller;
+
+import com.oasis.ocrspring.repository.ImageRepository;
+import com.oasis.ocrspring.repository.PatientRepository;
+import com.oasis.ocrspring.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/dashboard")
+public class Dashboard {
+
+    @Autowired
+    private UserRepository UserRepo;
+    @Autowired
+    private ImageRepository imageRepo;
+    @Autowired
+    private PatientRepository PatientRepo;
+    @ApiIgnore
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+    @GetMapping("/percentages")
+    public String riskHabitPercentage(){
+        return "/api/dashboard/percentages";
+
+    }
+    @GetMapping("/totals")
+    public String getTotal(){
+        return "/api/dashboard/totals";
+    }
+}
