8ae4c165f801786e0a7381abdbf638b00963a84a
Tharaka Dilshan <e20069@eng.pdn.ac.lk>
2024-08-15 10:54:10 +0530

    [115] updatedsome parts

--- ocrspring/src/main/java/com/oasis/ocrspring/controller/PatientController.java
+++ ocrspring/src/main/java/com/oasis/ocrspring/controller/PatientController.java
@@ -7,6 +7,7 @@ import com.oasis.ocrspring.service.ResponseMessages.ErrorMessage;
 import com.oasis.ocrspring.service.auth.AuthenticationToken;
 import com.oasis.ocrspring.service.auth.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -69,9 +70,49 @@ public class PatientController {
 
     //get all patients
     @GetMapping("/get")
-    public List<com.oasis.ocrspring.model.Patient> getPatient() {
-        //todo : should add user id and his authentication checking
-        return patientService.allPatientDetails();
+    public ResponseEntity<?> getPatient(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam(required = false) String search,
+            @RequestParam(required = false) String page,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false)String filter
+    ) throws IOException {
+        authenticationToken.authenticateRequest(request, response);
+
+        if(!tokenService.checkPermissions(request, Collections.singletonList("300"))){
+            return ResponseEntity.status(401).body(new ErrorMessage("Unauthorized Access"));
+        }
+
+        String clinicianId=request.getAttribute("_id").toString();
+
+        int pageSize = 20;
+        int pageQuery = page == null ? 1 : Integer.parseInt(page);
+        String searchQuery = search == null ? "" : search;
+        Sort.Direction sortDirection = sort == null || sort.equals("false") ? Sort.Direction.DESC : Sort.Direction.ASC;
+
+        Sort Order;
+        if(!(filter==null)){
+             Order = Sort.by(sortDirection, filter);
+        }else{
+             Order = Sort.by(sortDirection, "patientId");
+        }
+
+        List<Patient> patients;
+
+        try{
+            if(searchQuery.isEmpty()){
+                patients = patientService.getAllPatients(clinicianId,pageQuery,pageSize,Order);
+            }else{
+                patients = patientService.searchPatient(clinicianId,searchQuery,pageQuery,pageSize,Order);
+            }
+            return ResponseEntity.ok(patients);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage("Internal Server Error"));
+        }
+
+
+
     }
 
 
